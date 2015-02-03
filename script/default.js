f0d50d4305e1b0f1c15eb9c3ab4d0b68aaeec559
JSANN <zenwink@gmail.com>
2015-02-03 22:43:55 +0800

    add typed

---
+++ script/default.js
@@ -0,0 +1,14 @@
+(function() {
+  $(function() {
+    return $(".js-typed").typed({
+      strings: ["hudie feibuguo", "蝴蝶飞不过沧海，谁忍心责怪？"],
+      typeSpeed: 100,
+      callback: function() {
+        return $(".typed-cursor").fadeOut(function() {
+          return $(this).remove();
+        });
+      }
+    });
+  });
+
+}).call(this);
