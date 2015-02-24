f2e65575d5c1cb6abb0c5c55a6a727f1f7c488e1
JSANN <zenwink@gmail.com>
2015-02-24 11:13:58 +0800

    remove post page banner

--- script/default.js
+++ script/default.js
@@ -1,14 +1,16 @@
 (function() {
   $(function() {
-    return $(".js-typed").typed({
-      strings: ["hudie feibuguo", "蝴蝶飞不过沧海，谁忍心责怪？"],
-      typeSpeed: 100,
-      callback: function() {
-        return $(".typed-cursor").fadeOut(function() {
-          return $(this).remove();
-        });
-      }
-    });
+    if ($(".js-typed").length) {
+      return $(".js-typed").typed({
+        strings: ["hudie feibuguo", "蝴蝶飞不过沧海，谁忍心责怪？"],
+        typeSpeed: 100,
+        callback: function() {
+          return $(".typed-cursor").fadeOut(function() {
+            return $(this).remove();
+          });
+        }
+      });
+    }
   });
 
 }).call(this);
